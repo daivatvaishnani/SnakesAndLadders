696bb74cf71586f5b8b8dbd0d31c207f5c83309d
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2020-02-28 18:36:35 +0530

    Initial design of the Game

---
+++ src/drivers/GameDriver.java
@@ -0,0 +1,39 @@
+package drivers;
+
+import models.Ladder;
+import models.Player;
+import models.Snake;
+import services.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GameDriver {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numberOfSnakes = sc.nextInt();
+		List<Snake> snakes = new ArrayList<>();
+		for(int i = 0; i < numberOfSnakes; ++i) {
+			int head = sc.nextInt();
+			int tail = sc.nextInt();
+			snakes.add(new Snake(head, tail));
+		}
+		int numberOfLadders = sc.nextInt();
+		List<Ladder> ladders = new ArrayList<>();
+		for(int i = 0; i < numberOfLadders; ++i) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			ladders.add(new Ladder(start, end));
+		}
+		int numberOfPlayers = sc.nextInt();
+		List<Player> players = new ArrayList<>();
+		for(int i = 0; i < numberOfPlayers; ++i) {
+			String name = sc.next();
+			players.add(new Player(name));
+		}
+		Game game = new Game(snakes, ladders, players);
+		game.startGame();
+		int a = 10;
+	}
+}
